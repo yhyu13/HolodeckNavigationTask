827065a86181b29f699a399ccd4be68166ed6339
Kevin <kevin.horecka@gmail.com>
2016-02-04 15:09:50 -0600

    Release Build v3_1
    Testing on build complete.
    
    -Added secret button (numpad +) to teleport forward in case of issues.
    -Made object placement at test slightly farther away.
    -Made test objects walk-through-able.

---
+++ assets/TeleportForwardSecretButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportForwardSecretButton : MonoBehaviour {
+    
+    public KeyCode secretButton = KeyCode.KeypadPlus;
+    private bool previousState = false;
+
+    public float teleportDistance = 2f;
+	// Update is called once per frame
+	void Update () {
+        bool currentState = Input.GetKey(secretButton);
+        bool risingEdge = !previousState && currentState;
+        if (risingEdge)
+            transform.position += transform.forward * teleportDistance;
+        previousState = currentState;
+	}
+}
